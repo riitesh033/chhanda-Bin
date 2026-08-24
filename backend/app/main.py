91c0b74ed470f28b4b29f6b9d77a23e3fdf9b664
Ritesh Gupta <riitesh033@gmail.com>
2026-08-24 21:52:24 +0530

    fix production cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,6 +12,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
+                   "http://localhost:5174",
                    "https://chhanda-bin-frontend.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
