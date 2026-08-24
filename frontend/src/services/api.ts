c9ce6789a605d77842c47231cb021017a3065a84
Ritesh Gupta <riitesh033@gmail.com>
2026-08-24 21:42:56 +0530

    Fixed

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -4,10 +4,6 @@ const api = axios.create({
   baseURL:
     import.meta.env.VITE_API_BASE_URL ||
     "http://localhost:8000/api",
-
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 export default api;
